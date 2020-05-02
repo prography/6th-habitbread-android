10238219f6c4d132dddf23d64912013f09496370
chooeeh <chchgml10@gmail.com>
2020-05-02 15:53:17 +0900

    Implement Features : Bottom Navigation

--- HabitBread/app/src/main/java/com/example/habitbread/MainActivity.kt
+++ HabitBread/app/src/main/java/com/example/habitbread/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.habitbread
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
+
+        replaceFragment(MyHabits())
+        main_bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.myHabits -> {
+                    replaceFragment(MyHabits())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.ranking -> {
+                    replaceFragment(Ranking())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.account -> {
+                    replaceFragment(Account())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
+
     }
+
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
