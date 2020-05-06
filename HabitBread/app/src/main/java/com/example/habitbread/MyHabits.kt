351b90f7cf0b41d0f20856da3e625958e158b608
chooeeh <chchgml10@gmail.com>
2020-05-06 17:52:25 +0900

    Refactor Codes : Modify some codes as shown PR feedbacks.

--- HabitBread/app/src/main/java/com/example/habitbread/MyHabits.kt
+++ HabitBread/app/src/main/java/com/example/habitbread/MyHabits.kt
@@ -1,13 +1,12 @@
 package com.example.habitbread
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_my_habits.*
 
 class MyHabits : Fragment() {
 
@@ -19,4 +18,14 @@ class MyHabits : Fragment() {
         return inflater.inflate(R.layout.fragment_my_habits, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        showHabitRegisterPage()
+    }
+
+    private fun showHabitRegisterPage(){
+        button_registerHabit.setOnClickListener {
+            Toast.makeText(activity, "등록 버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
