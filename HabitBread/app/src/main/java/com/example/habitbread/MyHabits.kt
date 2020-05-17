33d4da800e562f0633db872ba95350551d51b498
chooeeh <chchgml10@gmail.com>
2020-05-18 00:12:38 +0900

    Implement Features : Registration Bottom Sheet Fragment

--- HabitBread/app/src/main/java/com/example/habitbread/MyHabits.kt
+++ HabitBread/app/src/main/java/com/example/habitbread/MyHabits.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.fragment_my_habits.*
 
 class MyHabits : Fragment() {
--- HabitBread/app/src/main/java/com/example/habitbread/RegistrationBottomSheet.kt
+++ HabitBread/app/src/main/java/com/example/habitbread/RegistrationBottomSheet.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import kotlinx.android.synthetic.main.fragment_my_habits.*
 
 class RegistrationBottomSheet : BottomSheetDialogFragment() {
     override fun onCreateView(
