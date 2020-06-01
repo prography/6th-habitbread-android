7952ab7800473131fbaea1b92ef01e9ec35c7aaa
chooeeh <chchgml10@gmail.com>
2020-06-02 03:39:12 +0900

    Implement Features : Implement Detail Fragment UI xml file

---
+++ HabitBread/app/src/main/java/com/example/habitbread/ui/activity/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.habitbread.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.habitbread.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
