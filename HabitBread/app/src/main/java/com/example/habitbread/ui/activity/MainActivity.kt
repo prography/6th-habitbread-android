05dd8b454e03007be3a27f5b3cb052db6a9ee5b8
chooeeh <chchgml10@gmail.com>
2020-06-02 15:36:19 +0900

    Refactor Codes : set Firebase Cloud Messaging service

--- HabitBread/app/src/main/java/com/example/habitbread/ui/activity/MainActivity.kt
+++ HabitBread/app/src/main/java/com/example/habitbread/ui/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.habitbread.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.habitbread.R
@@ -10,12 +11,19 @@ import com.example.habitbread.ui.fragment.Account
 import com.example.habitbread.ui.viewModel.HabitViewModel
 import com.example.habitbread.ui.fragment.MyHabits
 import com.example.habitbread.ui.fragment.Ranking
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     private val viewModel: HabitViewModel by viewModel()
+
+    override fun onResume() {
+        super.onResume()
+        getFCMCurrentToken()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,5 +56,18 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.main_frameLayout, fragment)
         fragmentTransaction.commit()
+    }
+
+    private fun getFCMCurrentToken(){
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener(OnCompleteListener { task ->
+            if(!task.isSuccessful){
+                Log.w("FCMCurrentTokenTest", "getFCMCurrentToken failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // This is a new Token(Instance ID)
+            val token = task.result?.token
+            Log.d("chohee", token.toString())
+        })
     }
 }
