796c4e7aeda79f0b4294ea7ba4df077703b34a40
chooeeh <chchgml10@gmail.com>
2020-06-04 21:25:44 +0900

    Refactoring Codes : change Category recyclerView to chips

--- HabitBread/app/src/main/java/com/example/habitbread/ui/fragment/RegistrationBottomSheet.kt
+++ HabitBread/app/src/main/java/com/example/habitbread/ui/fragment/RegistrationBottomSheet.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.habitbread.R
+import com.example.habitbread.adapter.AlarmWeekAdapter
 import com.example.habitbread.adapter.CategoryAdapter
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.ChipGroup
 import kotlinx.android.synthetic.main.fragment_registraion.*
 
 class RegistrationBottomSheet : BottomSheetDialogFragment() {
 
     private lateinit var rvCategory: RecyclerView
     private lateinit var adapterCategory: CategoryAdapter
+
+    private lateinit var rvAlarmWeekDay: RecyclerView
+    private lateinit var adapterAlarmWeekDay: AlarmWeekAdapter
+
+    private lateinit var chipGroupCategory: ChipGroup
 
     override fun getTheme(): Int {
         return R.style.bottomSheetDialogTheme
@@ -29,27 +32,47 @@ class RegistrationBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_registraion, container, false)
-        rvCategory = view!!.findViewById(R.id.recyclerView_category)
+        chipGroupCategory = view.findViewById(R.id.chipGroup_category)
+//        rvCategory = view.findViewById(R.id.recyclerView_category)
+//        rvAlarmWeekDay = view.findViewById(R.id.recyclerView_alarm)
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        initRvCategory()
+        //initRecyclerView()
+        handlingCategoryChips()
         imageView_close.setOnClickListener {
             dismiss()
         }
     }
 
-    private fun initRvCategory(){
-        adapterCategory = CategoryAdapter(context)
-        rvCategory.adapter = adapterCategory
-        rvCategory.layoutManager = GridLayoutManager(context, 3)
-        adapterCategory.data = initCategoryData
-        adapterCategory.notifyDataSetChanged()
+//    private fun initRecyclerView(){
+//        // category recyclerView
+//        adapterCategory = CategoryAdapter(context)
+//        rvCategory.adapter = adapterCategory
+//        rvCategory.layoutManager = GridLayoutManager(context, 3)
+//        adapterCategory.data = initCategoryData
+//        adapterCategory.notifyDataSetChanged()
+//
+//        // alarm weekday recyclerView
+//        adapterAlarmWeekDay = AlarmWeekAdapter(context)
+//        rvAlarmWeekDay.adapter = adapterAlarmWeekDay
+//        rvAlarmWeekDay.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+//        adapterAlarmWeekDay.data = initWeekDayData
+//        adapterAlarmWeekDay.notifyDataSetChanged()
+//    }
+
+    fun handlingCategoryChips(){
+        chipGroupCategory.setOnCheckedChangeListener { group, checkedId ->
+        }
     }
 
     private val initCategoryData = listOf(
         "건강", "공부", "운동", "생활", "취미", "기타"
+    )
+
+    private val initWeekDayData = listOf(
+        "월", "화", "수", "목", "금", "토", "일"
     )
 }
