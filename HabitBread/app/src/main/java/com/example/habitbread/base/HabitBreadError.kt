22bc540fb8de0669925e6d78398b5f6ea6ebbcaf
Capri.Yeom <capri.yeom@sendbird.com>
2020-06-11 14:50:30 +0900

    Implement Features: Add Error Class ( how to use : to use exception, give codes when some errors rightly )

---
+++ HabitBread/app/src/main/java/com/example/habitbread/base/HabitBreadError.kt
@@ -0,0 +1,7 @@
+package com.example.habitbread.base
+
+class HabitBreadError {
+    val ERR_INVALID_PARAMETER_STRING = 400100
+    val ERR_INVALID_PARAMETER_NUMBER = 400101
+    val ERR_INVALID_PARAMETER_VALUE = 400111;
+}
