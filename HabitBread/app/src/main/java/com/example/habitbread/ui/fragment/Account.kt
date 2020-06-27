f21a748ed5f459ae039c60e275d9f2f1157708d8
capriYeom <capri.yeom@sendbird.com>
2020-06-27 15:47:06 +0900

    Implement Feature : Finish Ranking View

--- HabitBread/app/src/main/java/com/example/habitbread/ui/fragment/Account.kt
+++ HabitBread/app/src/main/java/com/example/habitbread/ui/fragment/Account.kt
@@ -1,8 +1,20 @@
 package com.example.habitbread.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.habitbread.R
-import com.example.habitbread.base.BaseFragment
 
 class Account : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view= inflater.inflate(R.layout.fragment_account, container, false)
+        return view
+    }
 }
