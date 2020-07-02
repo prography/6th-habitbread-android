300ccd875e3a24deb12b696e0e191f7c94d7e51f
capriYeom <capri.yeom@sendbird.com>
2020-07-02 17:37:42 +0900

    Refactor Codes : Remove unused Handler

--- HabitBread/app/src/main/java/com/example/habitbread/interface/HabitListHandler.kt
+++
@@ -1,7 +0,0 @@
-package com.example.habitbread.`interface`
-
-import com.example.habitbread.data.Habits
-
-interface HabitListHandler {
-    fun onResult(handlerList: List<Habits>?)
-}
--- HabitBread/app/src/main/java/com/example/habitbread/interface/UpdateFinishHandler.kt
+++
@@ -1,5 +0,0 @@
-package com.example.habitbread.`interface`
-
-interface UpdateFinishHandler {
-    fun onUpdated();
-}
