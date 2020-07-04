547dfa20891b65e9649e8d4e089172a1dc01c449
capriYeom <capri.yeom@sendbird.com>
2020-07-04 15:00:19 +0900

    Implement Features : Add Google OAuth Features

--- HabitBread/app/src/main/java/com/example/habitbread/ui/activity/SplashActivity.kt
+++ HabitBread/app/src/main/java/com/example/habitbread/ui/activity/SplashActivity.kt
@@ -1,17 +1,11 @@
 package com.example.habitbread.ui.activity
 
-import com.example.habitbread.data.GoogleOAuthRequest
-import kotlinx.coroutines.runBlocking
-import retrofit2.*
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.habitbread.R
 import com.example.habitbread.api.ServerImpl
+import com.example.habitbread.data.GoogleOAuthRequest
 import com.example.habitbread.data.GoogleOAuthResponse
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -19,44 +13,27 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-
-class LoginActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     private val TAG = "HabitBread"
-    var client: GoogleSignInClient? = null
+    private var client : GoogleSignInClient? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
         val gso =
             GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken("191839451290-81q6qni5lt1s5nad9lfrhahabjtrp2pa.apps.googleusercontent.com")
                 .requestEmail()
                 .build()
         client = GoogleSignIn.getClient(this, gso)
-        button_google_sign_in.setOnClickListener(View.OnClickListener { signIn() })
     }
 
-    public override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 9001) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task =
-                GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-        }
-    }
-
-    private fun signIn() {
-        val signInIntent = client!!.signInIntent
-        startActivityForResult(signInIntent, 9001)
+    override fun onStart() {
+        super.onStart()
+        val googleResponse : Task<GoogleSignInAccount> = client!!.silentSignIn()
+        handleSignInResult(googleResponse)
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
@@ -77,6 +54,17 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun updateUi(idToken: String?) {
+        var intent : Intent? = null;
+        if (idToken != null) {
+            intent = Intent(this@SplashActivity, MainActivity::class.java)
+        } else {
+            intent = Intent(this@SplashActivity, LoginActivity::class.java)
+        }
+        startActivity(intent)
+        finish()
+    }
+
     private fun sendGoogleOauth(idToken: String?) {
         val tempRequest = GoogleOAuthRequest(idToken!!);
         val call = ServerImpl.APIService.serverLoginWithGoogle(tempRequest);
@@ -87,16 +75,7 @@ class LoginActivity : AppCompatActivity() {
             ) {
                 Log.d(TAG, "onResponse: ${response.body()}")
                 val googleOauthResponse = response.body();
-                if (googleOauthResponse?.idToken != null) {
-                    Log.d(TAG, googleOauthResponse!!.idToken)
-                    val intent :Intent = Intent(this@LoginActivity, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    Log.d(TAG, googleOauthResponse.toString());
-                    Toast.makeText(this@LoginActivity, "Google Oauth Failed", Toast.LENGTH_SHORT).show();
-                }
-
+                updateUi(googleOauthResponse?.idToken);
             }
             override fun onFailure(
                 call: Call<GoogleOAuthResponse?>,
